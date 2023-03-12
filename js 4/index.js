cb80e8c071ff68c34a3fd2d8d93c8dbf5915c540
Samiaerum <121792974+Samiaerum@users.noreply.github.com>
2023-03-12 15:00:25 +0500

    Add files via upload

---
+++ js 4/index.js
@@ -0,0 +1,35 @@
+// //        <----QUESTION#1---->
+
+// var w="afifa", x="mehar" ,y="hina" ,z="hani";
+
+
+// //        <----QUESTION#2---->
+
+// // Legal variable..
+
+// var firstName="samia";
+// var _age="19";
+// var $price="infinty";
+// var fullName="samia erum";
+// var numStudent=10;
+
+// // Legal variable..
+// var 123="10";
+// var my name="samia";
+// var var="wrong";
+// var my-name="samia";
+// var true="1";
+
+
+
+//        <----QUESTION#3---->
+const heading = document.getElementById("heading");
+heading.textContent = "Rules for naming JS variables";
+
+const paragraph = document.getElementById("paragraph");
+paragraph.innerHTML = `Variable names can only contain <strong>letters</strong>, <strong>numbers</strong>, <strong>underscores</strong>, and <strong>dollar signs</strong>.<br>
+For example <code>$my_1stVariable</code><br>
+Variables must begin with a <strong>letter</strong>, <strong>underscore</strong>, or <strong>dollar sign</strong>.<br>
+For example <code>$name</code>, <code>_name</code>, or <code>name</code><br>
+Variable names are case <strong>sensitive</strong>.<br>
+Variable names should not be JS <strong>keywords</strong>.`;
